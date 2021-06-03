25b99c41d211a91029ba85acd5a025e67fb82163
GRoman <grromman@gmail.com>
2021-06-03 21:03:12 +0500

    Initial Commit

---
+++ GB_Map/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  GB_Map
+//
+//  Created by admin on 03.06.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'GB_Map' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for GB_Map
+	pod 'GoogleMaps', '5.0.0'
+
+end
